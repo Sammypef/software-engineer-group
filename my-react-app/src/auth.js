5422f2dbf6a741ddd55c284c5be455d0fb96cbde
sktt04 <chayadaguild@gmail.com>
2025-10-10 16:04:16 +0700

    - attempt2 trying to remove keys

--- my-react-app/src/auth.js
+++ my-react-app/src/auth.js
@@ -1,16 +1,18 @@
+//auth.js
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const GOOGLE_CLIENT_ID = '45664116241-i48nvb4q7f947hhpa6jr1asrmrk60nn5.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
 
 passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:5000/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
+
 
 passport.serializeUser((user, done) => {
   done(null, user);
